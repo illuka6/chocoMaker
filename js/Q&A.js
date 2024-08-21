5028de50dfdb5807ff7a619d1d1a136298291d8b
illuka6 <06eri0109@gmail.com>
2024-08-21 11:22:14 +0800

    qna_list_slide_down效果

--- js/Q&A.js
+++ js/Q&A.js
@@ -1,16 +1,11 @@
-
+/* 
 
 function toggleDropdown(btnClass, listClass) {
-
-
 
     let btns = document.getElementsByClassName(btnClass);
     let lists = document.getElementsByClassName(listClass);
 
     for (let i = 0; i < btns.length; i++) {
-
-
-
 
         btns[i].addEventListener('click', function () {
             
@@ -24,7 +19,33 @@ function toggleDropdown(btnClass, listClass) {
                 }
             }
         )};
+} */
+
+
+
+
+function toggleDropdown(btnClass, listClass) {
+
+    let btns = document.getElementsByClassName(btnClass);
+    let lists = document.getElementsByClassName(listClass);
+    // let vector = document.getElementsByClassName(vector);
+
+    for (let i = 0; i < btns.length; i++) {
+        btns[i].addEventListener('click', function () {
+            
+                let list = lists[i];
+                // 获取当前的 display 属性
+
+                list.classList.toggle('expanded');
+                
+               /*  let currentDisplay = list.style.display;
+                if (currentDisplay === 'none' || currentDisplay === '') {
+                    list.style.display = 'block'; // 显示下拉菜单
+                } else {
+                    list.style.display = 'none'; // 隐藏下拉菜单
+                } */
+            }
+        )};
 
     
 }
-// toggleDropdown('question', 'ans'); 要放在react內容,指定執行時機(渲染完畢)
