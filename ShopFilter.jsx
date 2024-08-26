fb36bfee00cb28b2cbe6228af1d61bca78ecd2da
illuka6 <06eri0109@gmail.com>
2024-08-26 18:05:53 +0800

    filter功能完成

--- ShopFilter.jsx
+++ ShopFilter.jsx
@@ -1,39 +1,35 @@
-function ShopFilter({ handlePriceFilter, toggleDropdown, showPriceDropdown, showCountsDropdown, showFestivalDropdown }) {
+function ShopFilter({ handleFestivalFilter,handlePriceFilter, handleCountsFilter, toggleDropdown, showPriceDropdown, showCountsDropdown, showFestivalDropdown ,showPermutationDropdown }) {
 
 
     return (<>
         <div id="filter" class="filter">
             <div class="filter_list">
                 <ul class="drop-down-menu">
-                    <li id="festivalBtn"><img class="BtnPic" src="./images/shop_images/filter_btn1_down.svg" alt="" onClick={() => toggleDropdown('festival')} />
+                    <li id="festivalBtn" onClick={() => toggleDropdown('festival')} ><img class="BtnPic" src="./images/shop_images/filter_btn1_down.svg" alt="" />
                         {showFestivalDropdown && (
                         <ul id="festivalList">
-                            <li><a href="#">情人節</a>
+                            <li onClick={() => handleFestivalFilter('valentines')}><a>情人節</a>
                             </li>
-                            <li><a href="#">生日</a>
+                            <li onClick={() => handleFestivalFilter('birthday')}><a>生日</a>
                             </li>
-                            <li><a href="#">畢業</a>
+                            <li onClick={() => handleFestivalFilter('graduation')}><a>畢業</a>
                             </li>
-                            <li><a href="#">其它節慶</a>
+                            <li onClick={() => handleFestivalFilter('otherDay')}><a>其它節慶</a>
                             </li>
                         </ul>
                         )}
                     </li>
-                    <li id="countsBtn"><img class="BtnPic" src="./images/shop_images/filter_btn2_down.svg" alt="" onClick={() => toggleDropdown('counts')} />
+                    <li id="countsBtn"  onClick={() => toggleDropdown('counts')}><img class="BtnPic" src="./images/shop_images/filter_btn2_down.svg" alt="" />
                     {showCountsDropdown  && (
                         <ul id="countsList">
-                            <li><a href="#">4入</a>
-                            </li>
-                            <li><a href="#">6入</a>
-                            </li>
-                            <li><a href="#">12入</a>
-                            </li>
-                            <li><a href="#">24入</a>
-                            </li>
+                            <li onClick={() => handleCountsFilter('are4')}><a>4入</a></li>
+                            <li onClick={() => handleCountsFilter('are6')}><a>6入</a></li>
+                            <li onClick={() => handleCountsFilter('are12')}><a>12入</a></li>
+                            <li onClick={() => handleCountsFilter('are24')}><a>24入</a></li>
                         </ul>
                     )}
                     </li>
-                    <li id="priceBtn"><img class="BtnPic" src="./images/shop_images/filter_btn3_down.svg" alt="" onClick={() => toggleDropdown('price')} />
+                    <li id="priceBtn"  onClick={() => toggleDropdown('price')} ><img class="BtnPic" src="./images/shop_images/filter_btn3_down.svg" alt=""/>
                         {showPriceDropdown && (
                             <ul id="priceList">
                                 <li onClick={() => handlePriceFilter('below500')}><a >500元以下</a></li>
@@ -46,14 +42,16 @@ function ShopFilter({ handlePriceFilter, toggleDropdown, showPriceDropdown, show
                 </ul>
             </div>
             <div class="permutation">
-                <div class="permutation_btn" id="permutationBtn">
-                    <a><img src="./images/shop_images/permutation.svg" alt="" /></a>
+                <div class="permutation_btn" id="permutationBtn" onClick={() => toggleDropdown('permutation')}>
+                    <img src="./images/shop_images/permutation.svg" alt="" />
+                    {showPermutationDropdown && (
                     <ul id="permutationList">
                         <li><a href="">熱門</a></li>
                         <li><a href="">推薦</a></li>
                         <li><a href="">價格由低到高</a></li>
                         <li><a href="">價格由高到低</a></li>
                     </ul>
+                    )}
                 </div>
             </div>
         </div>
