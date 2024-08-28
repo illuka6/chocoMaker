b239b99da611860b4dd5a76683fb2d55fe290aab
illuka6 <06eri0109@gmail.com>
2024-08-28 19:05:32 +0800

    首頁 shop名字更改 MENU FOOTER頁面串接

--- Footer.jsx
+++ Footer.jsx
@@ -5,9 +5,9 @@
                     <a className="logo" href="#"><img src="./images/footer_logo.svg" alt="" /></a>
                     <div className="bottom_nav">
                         <ul className="menu">
-                            <li><a href="./index3.html" id="home">首頁</a></li>
+                            <li><a href="./index.html" id="home">首頁</a></li>
                             <li><a href="#" id="news">最新消息</a></li>
-                            <li><a href="./shop_page2.html" id="shop">商品一覽</a></li>
+                            <li><a href="./shop.html" id="shop">商品一覽</a></li>
                             <li><a href="#" id="about">關於我們</a></li>
                             <li><a href="./QnA.html" id="Q&A">Q&A</a></li>
                             <li><a href="#" id="shopping_car">購物車</a></li>
