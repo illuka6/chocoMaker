99e1b1ca65f2cb075426a74b2b5d7d994bb9ff4b
illuka6 <06eri0109@gmail.com>
2024-08-28 16:29:11 +0800

    menu定位

--- Footer.jsx
+++ Footer.jsx
@@ -5,11 +5,11 @@
                     <a className="logo" href="#"><img src="./images/footer_logo.svg" alt="" /></a>
                     <div className="bottom_nav">
                         <ul className="menu">
-                            <li><a href="./index.html" id="home">首頁</a></li>
+                            <li><a href="./index3.html" id="home">首頁</a></li>
                             <li><a href="#" id="news">最新消息</a></li>
-                            <li><a href="./shop.html" id="shop">商品一覽</a></li>
+                            <li><a href="./shop_page2.html" id="shop">商品一覽</a></li>
                             <li><a href="#" id="about">關於我們</a></li>
-                            <li><a href="#" id="Q&A">Q&A</a></li>
+                            <li><a href="./QnA.html" id="Q&A">Q&A</a></li>
                             <li><a href="#" id="shopping_car">購物車</a></li>
                             <li><a href="#" id="member">會員專區</a></li>
                         </ul>
