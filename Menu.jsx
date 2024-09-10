ddf29f45b4035ead5966e1a9f87d2e4192ed6dbc
illuka6 <06eri0109@gmail.com>
2024-09-10 16:20:22 +0800

    menubar修改

--- Menu.jsx
+++ Menu.jsx
@@ -13,7 +13,7 @@ function Menu() {
                     </a></li>
                 </div>
                 <div id="menu_id_icon">
-                    <li><a href="#" id="menu_id_icon_search"><img
+                    <li><a id="menu_id_icon_search"><img
                         src="./images/Custom_block/menu/material-symbols-light_search.svg" alt="" /></a>
                     </li>
                     <li id="menu_id_icon_show"><a href="#"><img src="./images/Custom_block/menu/Vector.svg" alt="" /></a>
@@ -26,16 +26,16 @@ function Menu() {
                 <li id='menu_id_search_li'>
                     <div id="menu_id_search_divBox">
 
-                        <a href="#" id="menu_id_search_divBox_a1"><img src="./images/Custom_block/menu/search.svg"
+                        <a  id="menu_id_search_divBox_a1"><img src="./images/Custom_block/menu/search.svg"
                             alt="" />
                             {/* <input type="text" placeholder="搜尋" id="menu_id_search_divBox_a1_input"/> */}
                             <p>搜尋</p>
                         </a>
 
-                        <a href="#" id="menu_id_search_divBox_a2"><img src="./images/Custom_block/menu/Xicon.svg"
+                        <a id="menu_id_search_divBox_a2"><img src="./images/Custom_block/menu/Xicon.svg"
                             alt="" /></a>
                     </div>
-                    <li><a href="#" id="menu_id_search_li2"><img src="./images/Custom_block/menu/Vector.svg" alt="" /></a>
+                    <li><a id="menu_id_search_li2"><img src="./images/Custom_block/menu/Vector.svg" alt="" /></a>
                     </li>
                     <li><a href="./shopping_cart.html" id="menu_id_search_li3"><img
                         src="./images/Custom_block/menu/shopping-cart-basket-1--shopping-basket.svg" alt="" /></a>
