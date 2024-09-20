f6eeeaa408070b7d94feb1ec3e9d30ad75142e62
illuka6 <06eri0109@gmail.com>
2024-09-21 03:59:31 +0800

    首頁修改

--- js/index.js
+++ js/index.js
@@ -17,38 +17,38 @@ images.forEach(image => {
 /* quality */
 
 
-// 將字串拆分為單個字母並插入 HTML
-const textElement = document.getElementById('quality');
-const textContent = 'QUALITY';
-textElement.innerHTML = textContent.split('').map(char => `<span>${char}</span>`).join('');
+// // 將字串拆分為單個字母並插入 HTML
+// const textElement = document.getElementById('quality');
+// const textContent = 'QUALITY';
+// textElement.innerHTML = textContent.split('').map(char => `<span>${char}</span>`).join('');
 
-// 函數：為每個字母設置動畫延遲
-function setAnimationDelays() {
-    const spans = document.querySelectorAll('#quality span');
-    spans.forEach((span, index) => {
-        // 設置每個字母的動畫延遲
-        span.style.animationDelay = `${index * .33}s`; // 每個字母延遲 2 秒
-    });
-}
+// // 函數：為每個字母設置動畫延遲
+// function setAnimationDelays() {
+//     const spans = document.querySelectorAll('#quality span');
+//     spans.forEach((span, index) => {
+//         // 設置每個字母的動畫延遲
+//         span.style.animationDelay = `${index * .33}s`; // 每個字母延遲 2 秒
+//     });
+// }
 
-// 觸發字母翻轉動畫
-function triggerAnimation() {
-    const spans = document.querySelectorAll('#quality span');
-    spans.forEach(span => span.classList.add('flip'));
-}
+// // 觸發字母翻轉動畫
+// function triggerAnimation() {
+//     const spans = document.querySelectorAll('#quality span');
+//     spans.forEach(span => span.classList.add('flip'));
+// }
 
-// 設置動畫延遲並觸發動畫
-setAnimationDelays();
-// 假設你希望動畫在滾動到一定位置時觸發
-window.addEventListener('scroll', () => {
-    const rect = textElement.getBoundingClientRect();
-    const windowHeight = window.innerHeight;
+// // 設置動畫延遲並觸發動畫
+// setAnimationDelays();
+// // 假設你希望動畫在滾動到一定位置時觸發
+// window.addEventListener('scroll', () => {
+//     const rect = textElement.getBoundingClientRect();
+//     const windowHeight = window.innerHeight;
 
-    // 如果文本區域在視窗中，觸發動畫
-    if (rect.top < windowHeight * 0.9 && rect.bottom > windowHeight / 2) {
-        triggerAnimation();
-    }
-});
+//     // 如果文本區域在視窗中，觸發動畫
+//     if (rect.top < windowHeight * 0.9 && rect.bottom > windowHeight / 2) {
+//         triggerAnimation();
+//     }
+// });
 
 
 // JQuery Smoove
